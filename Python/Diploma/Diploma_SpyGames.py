edf4735386c60813686c57d40a3f3995882ae542
UsaMasU <44852266+UsaMasU@users.noreply.github.com>
2018-12-25 03:02:41 +0300

    Finally

--- Python/Diploma/Diploma_SpyGames.py
+++ Python/Diploma/Diploma_SpyGames.py
@@ -1,4 +1,3 @@
-from pprint import pprint
 import requests
 import json
 import time
@@ -19,12 +18,14 @@ auth_data = {
     'v': '5.52'
 }
 # активный токен
-#token = '9627011e638fd171aa683a0f3b507e10b93b7b1662d32b71d3d1f9e54681bee6b921ab2681bcf815c11c6'
-token = '925b144b540d411baa0f5c43ea6fbce1942a7e597df7348818bb12a7d248018b9ee2c6bbcbb4db3671c74'
+token = '8f9e5ab49db2fd32ab5d8ca59e548b3bec5aa0c4a66a2b91abebd683a228c1024ba2217a5efb226e6ac33'
 ver = '5.92'  # версия API
 
-# функция проверки актуальности токена
+
 def check_token():
+    """
+    функция проверки актуальности токена
+    """
     response = requests.get(M_URL + 'users.get', {'user_ids': '1', 'access_token': token, 'v': ver})
     conn_state = response.json()
     for state, val_state in conn_state.items():
@@ -33,7 +34,11 @@ def check_token():
                 return 0
     return 1
 
+
 class WorkTimer:
+    """
+    менеджер контекста для оценки времени выполнения
+    """
     time_start = 0
     time_finish = 0
     time_work = 0
@@ -47,20 +52,23 @@ class WorkTimer:
     def __enter__(self):
         self.time_start = time.time()
         time_in_format = time.strftime("%H.%M.%S", time.localtime())  # "%Y-%m-%d-%H.%M.%S"
-        print(self.name, ': Start operation time: ', time_in_format, sep='')
+        print(self.name, ': старт выполнения: ', time_in_format, sep='')
 
     def __exit__(self, time_start, time_finish, time_work):
         self.time_finish = time.time()
         time_in_format = time.strftime("%H.%M.%S", time.localtime())  # "%Y-%m-%d-%H.%M.%S"
-        print(self.name, ': Finish operation time: ', time_in_format, sep='')
+        print(self.name, ': стоп выполнения: ', time_in_format, sep='')
         self.time_work = self.time_finish - self.time_start
-        print(self.name, ': Operation lasts: {:.{prec}f} sec'.format(self.time_work, prec = 3), sep = '')
+        print(self.name, ': продолжительность: {:.{prec}f} sec'.format(self.time_work, prec=3), sep='')
 
-# класс пользователь VK
+
 class User:
+    """
+    класс пользователь VK
+    """
     global token, ver  # глобальные данные используемые во всех запросах
     obj_name = ''  # имя экземпляра
-    uid = '' # ID
+    uid = ''  # ID
     link_url = ''  # ссылка на профиль
     profile = {  # профиль
         'id': '',  # ID
@@ -71,11 +79,11 @@ class User:
         'sex': '',  # пол
         'status': ''  # статус
     }
-    friends = {}
-    groups = {}
+    friends = {}  # список друзей
+    groups = {}  # список групп
+    groups_data = {}  # подробные данные групп
 
-    # инициализация обьекта
-    def __init__(self, settings={}):  # {'obj_name': '', 'uid': '', 'profile_data': ''}
+    def __init__(self, settings={}):
         #  если имя в параметрах пропущено то задаем пустое имя экземпляру
         try:
             settings['obj_name']
@@ -108,26 +116,27 @@ class User:
         except KeyError:
             pass
 
-    # по умолчанию передача ссылки на профиль
     def __str__(self):
-        return str(self.link_url)
+        return str(self.link_url)  # передача ссылки на профиль
 
-    # вызов &
     def __and__(self, other):
-        # запрос общих друзей
-        response = self.get_cross_friends(self.uid, other.uid)
+        """
+        вызов &
+        """
+        response = self.get_cross_friends(self.uid, other.uid)  # запрос общих друзей
         return response
 
-    # запрос даннх профиля пользователя от VK
     def get_user(self, uid):
+        """
+        запрос даннх профиля пользователя от VK
+        """
         params = {
             'user_ids': uid,
             'fields': 'nickname,name,sex,bdate,status',
             'access_token': token,
             'v': ver
         }
-        # запрос данных VK
-        response = self.get_data('users.get', params)
+        response = self.get_data('users.get', params)  # запрос данных VK
         user_data = response.json()
         # полученные данные в цикле переписываем в профиль
         for key in self.profile.keys():
@@ -135,14 +144,14 @@ class User:
                 self.profile[key] = user_data['response'][0][key]
             except KeyError:
                 pass
-        # записываем ID
-        self.uid = str(user_data['response'][0]['id'])
-        # формируем ссылку на профиль VK
-        self.link_url = VK_ID_URL + self.uid
+        self.uid = str(user_data['response'][0]['id'])  # записываем ID
+        self.link_url = VK_ID_URL + self.uid  # формируем ссылку на профиль VK
         return user_data['response']
 
-    # заполнение профиля данными словаря
     def set_user(self, user_data):
+        """
+        заполнение профиля данными
+        """
         # формируем записываемые данные в зависимости от типа вызова метода
         if 'profile_data' in user_data:
             set_user_data = user_data['profile_data']
@@ -155,17 +164,15 @@ class User:
                 self.profile[key] = set_user_data[key]
             except KeyError:
                 pass
-        # записываем ID
-        self.uid = str(set_user_data['id'])
-        # формируем ссылку на профиль VK
-        self.link_url = VK_ID_URL + self.uid
+        self.uid = str(set_user_data['id'])  # записываем ID
+        self.link_url = VK_ID_URL + self.uid  # формируем ссылку на профиль VK
 
-    # запрос списка друзей пользователя VK
     def get_friends_list(self):
+        """
+        запрос списка друзей пользователя VK
+        """
         params = {
             'user_id': self.uid,
-            # 'order': 'name',
-            # 'fields': 'nickname,name,sex,bdate,status',
             'access_token': token,
             'v': ver
         }
@@ -175,86 +182,102 @@ class User:
         self.friends = friends_data['response']
         return self.friends
 
-    # запрос списка друзей пользователя VK
-    def get_groups_list(self, extended=0):
+    def get_groups_list(self):
+        """
+        запрос списка групп пользователя VK
+        """
         params = {
             'user_id': self.uid,
             'extended': '0',
-            'fields': 'members_count',
             'access_token': token,
             'v': ver
         }
-        # запрос данных VK
-        response = self.get_data('groups.get', params)
+        response = self.get_data('groups.get', params)  # запрос данных VK
         groups_data = response.json()
         self.groups = groups_data['response']
         return self.groups
 
-        # запрос списка друзей пользователя VK
-    def get_groups_list_extend(self):
+    def get_groups_data(self):
+        """
+        запрос подробных данных групп пользователя VK
+        """
         params = {
             'user_id': self.uid,
-            'extended': '0',
+            'extended': '1',
             'fields': 'members_count',
             'access_token': token,
             'v': ver
         }
-        # запрос данных VK
-        response = self.get_data('groups.get', params)
+        response = self.get_data('groups.get', params)  # запрос данных VK
         groups_data = response.json()
-        self.groups = groups_data['response']
-        return self.groups
+        self.groups_data = groups_data['response']
+        return self.groups_data
 
-    # запрос списка общих друзей в VK
     def get_cross_friends_list(self, src_uid, tar_uid):
+        """
+        запрос списка общих друзей в VK
+        """
         params = {
             'source_uid': src_uid,
             'target_uid': tar_uid,
             'access_token': token,
             'v': ver
             }
-        # запрос данных VK
-        response = self.get_data('friends.getMutual', params)
+        response = self.get_data('friends.getMutual', params)  # запрос данных VK
         users_data = response.json()
         return users_data['response']
 
-    # формирование экземпляров из списка общих друзей
     def get_cross_friends(self, src_uid, tar_uid):
+        """
+        формирование экземпляров из списка общих друзей
+        """
         cross_friends_obj = []
         cross_friends_list = self.get_cross_friends_list(src_uid, tar_uid)
         for i, friend in enumerate(cross_friends_list):
-            print(i + 1,': id ',friend, sep = '')
+            print(i + 1, ': id ', friend, sep='')
             # создания экземпляра и запись его в список экземпляров
             cross_friends_obj.append(User({'uid': friend}))
         return cross_friends_obj
 
-    def get_user_only_groups(self, start_group_num=0, stop_group_num=0):
+    def get_cross_groups(self, start_group_num=0, stop_group_num=0):
+        """
+        поиск общих групп пользователя и друзей
+        """
+        #  Настройка параметров границ анализа количества групп пользоателя
         if start_group_num != 0 and stop_group_num == 0:
             stop_group_num = start_group_num
-
         if start_group_num == 0 and stop_group_num == 0:
+            start_group_num = 1
             stop_group_num = self.groups['count']
-
         if stop_group_num < start_group_num:
             stop_group_num = self.groups['count']
         if start_group_num > stop_group_num:
-            start_group_num = 0
+            start_group_num = 1
 
-        print(start_group_num, stop_group_num)
+        print('\nАнализ групп пользователя:')
+        print('Группы с {} по {}:'.format(start_group_num, stop_group_num))
 
-        offset = 0
-        count = 24
-        cross_groups = []
-        friends_count = 0
+        offset = 0  # начальное смещение
+        count = 24  # Количество запросов внутри execute
+        cross_groups = []  # список пересекающихся групп
+        cross_friends = []  # список количества пересечений
+        friends_count = 0  # счечик по друзьям
+
+        # цикл перебора групп друзей
         for num_group, group in enumerate(self.groups['items']):
-
-            if num_group < start_group_num or num_group > stop_group_num:
+            #  если порядковый номер группы вне аналитического деапазона, то пропускаем
+            if num_group+1 < start_group_num or num_group+1 > stop_group_num:
                 continue
-
+            #  если перебрали всех друзей, то обнуляем счетчик и смещение
             if friends_count == self.friends['count']:
                 friends_count = 0
                 offset = 0
-            while(friends_count < self.friends['count']):
+            #  цикл перебора друзей с поисклм пересечений в группах.
+            #  выполняется на стороне VK методом execute.
+            #  1) запрос порции для анализа друзей (24 друга)
+            #  2) далее анализ этих 24 групп
+            while friends_count < self.friends['count']:
+                # параметры для запроса execute
                 params = {
                     'access_token': token,
                     'v': ver,
@@ -294,71 +317,121 @@ class User:
                     )
 
                 }
-
-                response = self.get_data('execute', params)
+                response = self.get_data('execute', params)  # вызов execute
 
                 try:
-                    exec_data= response.json()
-                    #pprint(exec_data)
-                    print(num_group, exec_data['response'])
-                    #pprint(exec_data['response'])
-                    #pprint(exec_data)
-                    offset = offset + exec_data['response'][1]
-                    friends_count = exec_data['response'][2]
-                    if exec_data['response'][3] > 0:
-                        cross_groups.append(exec_data['response'][0])
-                        friends_count = self.friends['count']
-                    #pprint(exec_data['response'])
-                except  KeyError:
-                    print('Error User')
-                finally:
-                    print(cross_groups)
-        return cross_groups
+                    exec_data = response.json()
+                    print(num_group+1, ') ID: ', exec_data['response'][0], '; проверено друзей: ', exec_data['response'][2], sep='')
 
-    # выполнение запроса к VK API
+                    offset = offset + exec_data['response'][1]  # увеличение смещения
+                    friends_count = exec_data['response'][2]  # изменение счетчика друзей пользователя
+
+                    # если обнаружено пересечение группы друга с группой пользовтеля
+                    if exec_data['response'][3] > 0:
+                        cross_groups.append(exec_data['response'][0])  # запись группы пересечения
+                        cross_friends.append(exec_data['response'][3])  # запись количесва друзей в этой группе
+                        # friends_count = self.friends['count']  # прекращаем дальнейший пойск по этой группе
+                        print('Обнаружена общая группа!')
+                except KeyError:
+                    print('Error key JSON')
+        return cross_groups, cross_friends
+
+    def show_cross_groups(self, cross_groups, cross_friends, max_friends=1):
+        """
+        подготовка данных по общим группам и количеству друзей в них
+        но не более чем max_friends
+        """
+        cross_groups_friends = {}  # общие группы пользователя и друзей
+        filter_cross_groups = {}  # общие группы пользователя и друзей, где друзей < max_friends
+        for num, group in enumerate(cross_groups):
+            try:
+                cross_groups_friends[group] += cross_friends[num]
+            except KeyError:
+                cross_groups_friends[group] = cross_friends[num]
+
+        for group in cross_groups_friends:
+            if cross_groups_friends[group] <= max_friends:
+                filter_cross_groups[group] = cross_groups_friends[group]
+
+        return filter_cross_groups
+
     def get_data(self, method_name='', params={}, delay_time=0.0):
+        """
+        выполнение запроса к VK API
+        """
         time.sleep(delay_time)  # пауза чтобы VK не заблокировал соединение
-        # строка запроса данных VK
-        response = requests.get(M_URL + method_name, params)
+        print('*')
+        response = requests.get(M_URL + method_name, params)  # строка запроса данных VK
         return response
+
 
 if __name__ == '__main__':
     # Проверка токена
     token_true = check_token()
-    if(token_true == 0):
-        print('Need new token')
+    if token_true == 0:
+        print('Необходим новый токен')
         exit(1)
 
-    with WorkTimer('Group analyze'):
-    # создание первого пользователя через конструктор
-        User_One = User({'uid': 'eshmargunov'}) # 412548 #eshmargunov
+    with WorkTimer('Поиск уникальных групп'):
+        # создание экземпляра пользователя
+        User_One = User({'uid': 'eshmargunov'})  # eshmargunov
         print(User_One.profile['first_name'], User_One.profile['last_name'])
-        print(User_One)
-        User_One.get_groups_list()
-        pprint(User_One.groups)
-        User_One.get_friends_list()
-        #print('friends', User_One.friends)
-        user_group_list = set()
-        cross_groups_list = set()
-        user_unique_list = set()
+        print(User_One)  # ссылка на профиль VK
+        User_One.get_groups_list()  # запрос списка групп
+        User_One.get_groups_data()  # запрос подробных данных групп
+        User_One.get_friends_list()  # запрос списка друзей
 
+        print('Количество друзей:', User_One.friends['count'])
+        print('Количество групп:', User_One.groups['count'])
+        for num, group in enumerate(User_One.groups['items']):
+            print(num+1, ') ', group, sep='')
+
+        # формирование множества групп пользователя
+        user_group_list = set()
         for group in User_One.groups['items']:
             user_group_list.add(group)
 
-        print('user group:', user_group_list)
+        # поиск общих с другими пользователями групп
+        cross_groups, cross_friends = User_One.get_cross_groups()
 
-        cross_groups = User_One.get_user_only_groups()
-        print(cross_groups)
+        max_friends = 3  # максимальное допустимое количкство друзей в общих группах
+        print('\nОбщие группы с количеством друзей не более:', max_friends)
+        cross_groups_friends = User_One.show_cross_groups(cross_groups, cross_friends, max_friends)
 
+        for num, cross_group in enumerate(cross_groups_friends.items()):
+            print(num+1, ') ', 'ID: ', cross_group[0], '; друзей: ', cross_group[1], sep='')
+
+        # формирование множества пересекающихся групп пользователя и групп его друзей
+        cross_groups_list = set()
         for group in cross_groups:
             cross_groups_list.add(group)
-        print('cross:', cross_groups_list)
 
+        #  формирование множества уникальных групп пользователя
+        user_unique_list = set()
         user_unique_list = user_group_list.difference(cross_groups_list)
-        print('unique group:', user_unique_list)
 
-        # with open("groups.json", "w", encoding="utf-8") as file:
-        #     json.dump(User_One.groups, file)
+        #  формирование данных для записи в JSON-файл
+        group_data_json = []  # список уникальных групп пользователя
+        for uniquie_group in user_unique_list:
+            for data in User_One.groups_data['items']:
+                group_data = {}  # словарь данных уникальной группы пользователя
+                if uniquie_group == data['id']:
+                    try:  # записываем необходиые данные уникальных групп
+                        group_data['gid'] = data['id']
+                        group_data['name'] = data['name']
+                        group_data['members_count'] = data['members_count']
+                    except KeyError:  # если попытка записи не удалась
+                        pass  # пропускаем группу
+                    finally:
+                        group_data_json.append(group_data)  # добавление в список уникальных групп
 
-    print(WorkTimer('Group analyze').time_work)
+        # сохранение уникальных групп пользователя в JSON-файл
+        with open("groups.json", "w", encoding="utf-8") as file:
+            json.dump(group_data_json, file)
 
+    # открываем созданный JSON-файл и считываем сохраненные группы
+    with open("groups.json", encoding="utf-8") as file_read:
+        json_data = json.load(file_read)
+    print('\nУникальные группы пользователя:')
+    for group in json_data:
+        print(group)
